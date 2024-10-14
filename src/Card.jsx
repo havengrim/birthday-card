4df6a6e27e06700ce8ac6bbaf7124ded97d9750c
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-10-14 16:21:30 +0800

    initial

---
+++ src/Card.jsx
@@ -0,0 +1,22 @@
+export default () => (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <h3 className="happy">HAPPY BIRTHDAY Love!</h3>
+        <div className="balloons">
+          <div className="balloonOne" />
+          <div className="balloonTwo" />
+          <div className="balloonThree" />
+          <div className="balloonFour" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <h3 className="back">Happy Birthday Love!!</h3>
+        <p>Dear Love,</p>
+        <p>
+          Happy birthday! I wish you all the best I love you so much, and I am so grateful to have you in my life. Happy Birthday! here's your coded card!!
+        </p>
+        <p className="name">Angelo</p>
+      </div>
+    </div>
+  );
+  
